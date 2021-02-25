519be6dfb01da227eda95981e3504f88e2459d59
Kakanaporn <skakanaporn@gmail.com>
2021-02-25 23:25:41 +0700

    Homework1&3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { HomeComponent } from './home/home.component';
 import { CountingComponent } from './counting/counting.component';
 import { FormsModule } from '@angular/forms';
 import { Lab4Component } from './lab4/lab4.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { SearchBarComponent } from './home/navbar/search-bar/search-bar.component';
+import { TopBannerComponent } from './home/top-banner/top-banner.component';
+import { CategoryMenuComponent } from './home/category-menu/category-menu.component';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +18,11 @@ import { Lab4Component } from './lab4/lab4.component';
     HomeComponent,
     CountingComponent,
     Lab4Component,
+    NavbarComponent,
+    SearchBarComponent,
+    TopBannerComponent,
+    CategoryMenuComponent,
+
   ],
   imports: [
     BrowserModule,
